2c81071f781a807e60ee9e82c9722f70f40ba3de
SimonRenggli1 <96227533+SimonRenggli1@users.noreply.github.com>
2023-05-31 12:02:56 +0200

    Update DupliScan.ps1

--- DupliScan.ps1
+++ DupliScan.ps1
@@ -14,104 +14,235 @@ Write-Host -ForegroundColor DarkMagenta "https://github.com/simonrenggli1/duplis
 Write-Host -ForegroundColor White "|_______|"
 Write-Host ""
 
-$partitions = Get-Partition | Where-Object {$_.Size -gt 1000000000}
-Write-Host ""
-Write-Host ""
-Write-Host -ForegroundColor Green "    Drive    Size"
-Write-Host -ForegroundColor Green "---------------------------------------------"
-
-foreach ($partition in $partitions) {
-    $size = "{0:N2} GB" -f ($partition.Size / 1GB)
-    $number = $partition.DiskNumber + 1
-    $partitionName = $partition.DriveLetter
-    Write-Host -ForegroundColor Cyan -NoNewline "$number"
-    Write-Host -ForegroundColor Green -NoNewline ".  "
-    Write-Host -ForegroundColor Green -NoNewline "$partitionName        "
-    Write-Host -ForegroundColor Cyan "$size"
+if ([System.Environment]::OSVersion.Version.Major -lt 6) {
+    Write-Host -ForegroundColor Red "[!] Unsupported operating system"
+    Write-Host ""
+    exit
 }
 
-Write-Host ""
-if ($number -eq 1)
-{
-    Write-Host -ForegroundColor Green -NoNewline "["
-    Write-Host -ForegroundColor Cyan -NoNewline "+"
-    Write-Host -ForegroundColor Green -NoNewline "]"
-    Write-Host -ForegroundColor Green -NoNewline " Select partition "
-    Write-Host -ForegroundColor Green -NoNewline "("
-    Write-Host -ForegroundColor Cyan -NoNewline "1"
-    Write-Host -ForegroundColor Green -NoNewline ")"
-    $partitionSelected = Read-Host " "
-} else {
-    Write-Host -ForegroundColor Green -NoNewline "["
-    Write-Host -ForegroundColor Cyan -NoNewline "+"
-    Write-Host -ForegroundColor Green -NoNewline "]"
-    Write-Host -ForegroundColor Green -NoNewline " Select partition "
-    Write-Host -ForegroundColor Green -NoNewline "("
-    Write-Host -ForegroundColor Cyan -NoNewline "1-$number"
-    Write-Host -ForegroundColor Green -NoNewline ")"
-    $partitionSelected = Read-Host " "
+if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    Write-Host -ForegroundColor Yellow -NoNewline "["
+    Write-Host -ForegroundColor Red -NoNewline "!"
+    Write-Host -ForegroundColor Yellow -NoNewline "]"
+    Write-Host -ForegroundColor Yellow -NoNewline " Warning: Recomended to run as"
+    Write-Host -ForegroundColor Red " administrator"
 }
 
-if ($partitionSelected -eq "")
-{
+Write-Host "    Mode"
+Write-Host "---------------------------------------------"
+Write-Host "    1. Scan partition"
+Write-Host "    2. Scan custom directory"
+Write-Host ""
+Write-Host -ForegroundColor Green -NoNewline "["
+Write-Host -ForegroundColor Cyan -NoNewline "+"
+Write-Host -ForegroundColor Green -NoNewline "]"
+Write-Host -ForegroundColor Green -NoNewline " Select partition "
+Write-Host -ForegroundColor Green -NoNewline "("
+Write-Host -ForegroundColor Cyan -NoNewline "1-2"
+Write-Host -ForegroundColor Green -NoNewline ")"
+$mode = Read-Host " "
+
+if ($mode -eq "") {
     Write-Host ""
     Write-Host -ForegroundColor Red "[!] No input"
     Write-Host ""
     exit
 }
 
-if ($partitionSelected -lt 1 -or $partitionSelected -gt $number)
-{
+if ($mode -lt 1 -or $mode -gt 2) {
     Write-Host ""
     Write-Host -ForegroundColor Red "[!] Out of range"
     Write-Host ""
     exit
 }
 
-$partitionSelected = $partitionSelected - 1
+if ($mode -eq 1) {
+    Write-Host ""
+    Write-Host -ForegroundColor Green -NoNewline "["
+    Write-Host -ForegroundColor Cyan -NoNewline "+"
+    Write-Host -ForegroundColor Green -NoNewline "]"
+    Write-Host -ForegroundColor Green " Scanning partitions..."
+    Write-Host ""
 
-Write-Host -ForegroundColor Green -NoNewline "["
-Write-Host -ForegroundColor Cyan -NoNewline "+"
-Write-Host -ForegroundColor Green -NoNewline "]"
-Write-Host -ForegroundColor Green " Scanning partition..."
-Write-Host ""
+    $partitions = Get-Partition | Where-Object { $_.Size -gt 1000000000 }
+    Write-Host ""
+    Write-Host -ForegroundColor Green "    Drive    Size"
+    Write-Host -ForegroundColor Green "---------------------------------------------"
 
-$driveLetter = $partitions[$partitionSelected].DriveLetter + ":"
-$files = try {
-    [System.IO.Directory]::EnumerateFiles($driveLetter, "*", [System.IO.SearchOption]::AllDirectories) | ForEach-Object {
-        try {
-            $_ -replace "^$([regex]::Escape($driveLetter))", ''
-        } catch {
-            Write-Host ""
-            Write-Host -ForegroundColor Red "[!] Error accessing file: $_"
-            Write-Host ""
+    foreach ($partition in $partitions) {
+        $size = "{0:N2} GB" -f ($partition.Size / 1GB)
+        $number = $partition.DiskNumber + 1
+        $partitionName = $partition.DriveLetter
+        Write-Host -ForegroundColor Cyan -NoNewline "$number"
+        Write-Host -ForegroundColor Green -NoNewline ".  "
+        Write-Host -ForegroundColor Green -NoNewline "$partitionName        "
+        Write-Host -ForegroundColor Cyan "$size"
+    }
+
+    Write-Host ""
+    if ($number -eq 1) {
+        Write-Host -ForegroundColor Green -NoNewline "["
+        Write-Host -ForegroundColor Cyan -NoNewline "+"
+        Write-Host -ForegroundColor Green -NoNewline "]"
+        Write-Host -ForegroundColor Green -NoNewline " Select partition "
+        Write-Host -ForegroundColor Green -NoNewline "("
+        Write-Host -ForegroundColor Cyan -NoNewline "1"
+        Write-Host -ForegroundColor Green -NoNewline ")"
+        $partitionSelected = Read-Host " "
+    }
+    else {
+        Write-Host -ForegroundColor Green -NoNewline "["
+        Write-Host -ForegroundColor Cyan -NoNewline "+"
+        Write-Host -ForegroundColor Green -NoNewline "]"
+        Write-Host -ForegroundColor Green -NoNewline " Select partition "
+        Write-Host -ForegroundColor Green -NoNewline "("
+        Write-Host -ForegroundColor Cyan -NoNewline "1-$number"
+        Write-Host -ForegroundColor Green -NoNewline ")"
+        $partitionSelected = Read-Host " "
+    }
+
+    if ($partitionSelected -eq "") {
+        Write-Host ""
+        Write-Host -ForegroundColor Red "[!] No input"
+        Write-Host ""
+        exit
+    }
+
+    if ($partitionSelected -lt 1 -or $partitionSelected -gt $number) {
+        Write-Host ""
+        Write-Host -ForegroundColor Red "[!] Out of range"
+        Write-Host ""
+        exit
+    }
+
+    $partitionSelected = $partitionSelected - 1
+
+    Write-Host -ForegroundColor Green -NoNewline "["
+    Write-Host -ForegroundColor Cyan -NoNewline "+"
+    Write-Host -ForegroundColor Green -NoNewline "]"
+    Write-Host -ForegroundColor Green " Scanning partition..."
+    Write-Host ""
+
+    $driveLetter = $partitions[$partitionSelected].DriveLetter + ":"
+    $files = try {
+        [System.IO.Directory]::EnumerateFiles($driveLetter, "*", [System.IO.SearchOption]::AllDirectories) | ForEach-Object {
+            try {
+                $_ -replace "^$([regex]::Escape($driveLetter))", ''
+            }
+            catch {
+                Write-Host ""
+                Write-Host -ForegroundColor Red "[!] Error accessing file: $_"
+                Write-Host ""
+            }
         }
     }
-} catch {
+    catch {
+        Write-Host ""
+        Write-Host -ForegroundColor Red "[!] Error accessing directory: $driveLetter"
+        Write-Host ""
+    }
+
     Write-Host ""
-    Write-Host -ForegroundColor Red "[!] Error accessing directory: $driveLetter"
+    Write-Host -ForegroundColor Green -NoNewline "["
+    Write-Host -ForegroundColor Cyan -NoNewline "+"
+    Write-Host -ForegroundColor Green -NoNewline "]"
+    Write-Host -ForegroundColor Green -NoNewline " Found "
+    Write-Host -ForegroundColor Cyan -NoNewline $files.Count
+    Write-Host -ForegroundColor Green " files"
+    Write-Host ""
+
+    Write-Host -ForegroundColor Green -NoNewline "["
+    Write-Host -ForegroundColor Cyan -NoNewline "+"
+    Write-Host -ForegroundColor Green -NoNewline "]"
+    Write-Host -ForegroundColor Green " Scanning for duplicates..."
+    Write-Host ""
+
+    $files | Group-Object -Property { $_ } | Where-Object { $_.Count -gt 1 } | ForEach-Object {
+        Write-Host -ForegroundColor Red "[!] Duplicate file: $($_.Name)"
+    }
+
+    Write-Host ""
+    Write-Host -ForegroundColor Green "[+] Done"
     Write-Host ""
 }
 
-Write-Host ""
-Write-Host -ForegroundColor Green -NoNewline "["
-Write-Host -ForegroundColor Cyan -NoNewline "+"
-Write-Host -ForegroundColor Green -NoNewline "]"
-Write-Host -ForegroundColor Green -NoNewline " Found "
-Write-Host -ForegroundColor Cyan -NoNewline $files.Count
-Write-Host -ForegroundColor Green " files"
-Write-Host ""
+if ($mode -eq 2) {
+    Write-Host -ForegroundColor Green -NoNewline "["
+    Write-Host -ForegroundColor Cyan -NoNewline "+"
+    Write-Host -ForegroundColor Green -NoNewline "]"
+    Write-Host -ForegroundColor Green -NoNewline " Enter custom directory "
+    $path = Read-Host " "
 
-Write-Host -ForegroundColor Green -NoNewline "["
-Write-Host -ForegroundColor Cyan -NoNewline "+"
-Write-Host -ForegroundColor Green -NoNewline "]"
-Write-Host -ForegroundColor Green " Scanning for duplicates..."
-Write-Host ""
+    # Check if path is empty
+    if ($path -eq "") {
+        Write-Host ""
+        Write-Host -ForegroundColor Red "[!] No input"
+        Write-Host ""
+        exit
+    }
 
-$files | Group-Object -Property { $_ } | Where-Object { $_.Count -gt 1 } | ForEach-Object {
-    Write-Host -ForegroundColor Red "[!] Duplicate file: $($_.Name)"
+    # Check if path exists
+    if (-not (Test-Path $path)) {
+        Write-Host ""
+        Write-Host -ForegroundColor Red "[!] Path does not exist"
+        Write-Host ""
+        exit
+    }
+
+    # Check if path is a directory
+    if (-not (Test-Path $path -PathType Container)) {
+        Write-Host ""
+        Write-Host -ForegroundColor Red "[!] Path is not a directory"
+        Write-Host ""
+        exit
+    }
+
+    Write-Host ""
+    Write-Host -ForegroundColor Green -NoNewline "["
+    Write-Host -ForegroundColor Cyan -NoNewline "+"
+    Write-Host -ForegroundColor Green -NoNewline "]"
+    Write-Host -ForegroundColor Green " Scanning directory..."
+
+    $fileInfo = @{}
+
+    function CheckDuplicate($filePath, $fileName, $fileSize) {
+        $key = "$fileName-$fileSize"
+        if ($fileInfo.ContainsKey($key)) {
+            $duplicatePaths = $fileInfo[$key]
+            $duplicatePaths += $filePath
+            $fileInfo[$key] = $duplicatePaths
+        }
+        else {
+            $fileInfo[$key] = @($filePath)
+        }
+    }
+
+    $files = Get-ChildItem -Path $path -File -Recurse
+    foreach ($file in $files) {
+        $filePath = $file.FullName
+        $fileName = $file.Name
+        $fileSize = $file.Length
+
+        CheckDuplicate -filePath $filePath -fileName $fileName -fileSize $fileSize
+    }
+
+    foreach ($key in $fileInfo.Keys) {
+        $duplicatePaths = $fileInfo[$key]
+        if ($duplicatePaths.Count -gt 1) {
+            Write-Host ""
+            Write-Host -ForegroundColor Yellow -NoNewline "["
+            Write-Host -ForegroundColor Cyan -NoNewline "+"
+            Write-Host -ForegroundColor Yellow -NoNewline "]"
+            Write-Host -ForegroundColor Yellow " Duplicate file: $key"
+
+            foreach ($duplicatePath in $duplicatePaths) {
+                Write-Host -ForegroundColor Red "[!] $duplicatePath"
+            }
+        }
+    }
+    
+    Write-Host ""
+    Write-Host -ForegroundColor Green "[+] Done"
+    Write-Host ""
+    exit
 }
-
-Write-Host ""
-Write-Host -ForegroundColor Green "[+] Done"
-Write-Host ""
